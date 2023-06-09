25319b27cb04f58d9c94f00634b48d32d324f255
evil-kekc <3371824@gmail.com>
2023-06-09 10:09:18 +0300

    Add admin part

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Post(models.Model):
@@ -16,6 +17,9 @@ class Post(models.Model):
     status = models.CharField(max_length=2,
                               choices=Status.choices,
                               default=Status.DRAFT)
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='blog_posts')
 
     class Meta:
         ordering = ['-publish']
