6bc9013856a29994c280aedab554fa19ef7b9d82
evil-kekc <3371824@gmail.com>
2023-06-09 13:15:34 +0300

    Add blog views and blog urls

--- blog/migrations/0001_initial.py
+++ blog/migrations/0001_initial.py
@@ -18,7 +18,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Post',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id_', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=250)),
                 ('slug', models.SlugField(max_length=250)),
                 ('body', models.TextField()),
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Post
+
+
+def post_list(request):
+    """
+
+    :param request:
+    :return:
+    """
+    posts = Post.objects.all()
+    return render(request,
+                  'blog/post/list.html',
+                  {
+                      'posts': posts
+                  })
+
+
+def post_detail(request, id_):
+    """Детальная информация о посте
+
+    :param request:
+    :param id_:
+    :return:
+    """
+    post = get_object_or_404(Post,
+                             id=id,
+                             status=Post.Status.PUBLISHED)
+
+    return render(request,
+                  'blog/post/detail.html',
+                  {
+                      'post': post
+                  })
