48df4691caf470a43ae0c66963934a4bec8c3948
evil-kekc <3371824@gmail.com>
2023-06-09 11:07:04 +0300

    Add custom model manager

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,11 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
+class PublisherManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
+
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
@@ -20,6 +25,9 @@ class Post(models.Model):
     author = models.ForeignKey(User,
                                on_delete=models.CASCADE,
                                related_name='blog_posts')
+
+    objects = models.Manager()
+    published = PublisherManager()
 
     class Meta:
         ordering = ['-publish']
