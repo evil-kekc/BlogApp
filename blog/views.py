09d4bcc8603fcb36210790f78d0c7ee78abc4aa5
evil-kekc <3371824@gmail.com>
2023-06-23 23:25:35 +0300

    Add sharing posts

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
+import os
+
+from django.core.mail import send_mail
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 
+from .forms import EmailPostForm
 from .models import Post
 
 
@@ -33,3 +37,39 @@ def post_detail(request, year, month, day, post):
                   {
                       'post': post
                   })
+
+
+def post_share(request, post_id):
+    """Sending a post by email
+
+    :param request: request object
+    :param post_id: post id
+    :return: HTTP Response
+    """
+    post = get_object_or_404(Post,
+                             id=post_id,
+                             status=Post.Status.PUBLISHED)
+
+    sent = False
+
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url()
+            )
+            subject = f"{cd['name']} recommended you read " \
+                      f"{post.title}"
+            message = f'Read {post.title} at {post_url}\n\n' \
+                      f"{cd['name']}\'s comments: {cd['comments']}"
+
+            send_mail(subject, message, os.getenv('ADMIN_GMAIL'), [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {
+        'post': post,
+        'form': form,
+        'sent': sent
+    })
--- mysite/settings.py
+++ mysite/settings.py
@@ -16,7 +16,6 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -28,7 +27,6 @@ SECRET_KEY = os.environ['SECRET_KEY']
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
@@ -73,7 +71,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
@@ -83,7 +80,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -103,7 +99,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -115,7 +110,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
@@ -125,3 +119,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = '3371824@gmail.com'
+EMAIL_HOST_PASSWORD = 'fjgmbjsqgmeapclp'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
