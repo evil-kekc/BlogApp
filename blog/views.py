f7c10ce43a16cd3e9a8940f89d9df5b27629db63
evil-kekc <3371824@gmail.com>
2023-06-23 22:11:50 +0300

    Add PostListView mixin

--- blog/views.py
+++ blog/views.py
@@ -1,32 +1,14 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
 
 from .models import Post
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
-def post_list(request):
-    """List of posts
-
-    :param request: request object
-    :return: HTTP Response
-    """
-    all_published_posts = Post.published.all()
-
-    paginator = Paginator(all_published_posts, 3)
-    page_number = request.GET.get('page', 1)
-
-    try:
-        posts = paginator.page(page_number)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)
-    except PageNotAnInteger:
-        posts = paginator.page(1)
-
-    return render(request,
-                  'blog/post/list.html',
-                  {
-                      'posts': posts
-                  })
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 
 def post_detail(request, year, month, day, post):
