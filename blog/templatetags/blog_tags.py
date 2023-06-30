f16c11cff46072e68ce027b19c0985f2bfe2fa17
evil-kekc <3371824@gmail.com>
2023-06-30 23:08:02 +0300

    Add simple tag returns QuerySet

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,4 +1,6 @@
 from django import template
+from django.db.models import Count
+
 from ..models import Post
 
 register = template.Library()
@@ -6,12 +8,33 @@ register = template.Library()
 
 @register.simple_tag
 def total_posts():
+    """Returns count of posts
+
+    :return: count of posts
+    """
     return Post.published.count()
 
 
 @register.inclusion_tag('blog/post/latest_posts.html')
 def show_latest_posts(count=5):
+    """Returns similar latest posts
+
+    :param count: count of posts
+    :return: latest posts
+    """
     latest_posts = Post.published.order_by('-publish')[:count]
     return {
         'latest_posts': latest_posts
     }
+
+
+@register.simple_tag
+def get_most_commented_posts(count=5):
+    """Returns most commented posts
+
+    :param count: count of posts
+    :return: most commented posts
+    """
+    return Post.published.annotate(
+        total_comments=Count('comments')
+    ).order_by('-total_comments')[:count]
