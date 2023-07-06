c991a3b91b7137244e3e288eff95d23da52d90b9
evil-kekc <3371824@gmail.com>
2023-07-06 13:53:53 +0300

    Add highlighting the stems of words and ranking results in search

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ import os
 from django.db.models import Count
 from django.core.mail import send_mail
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from django.shortcuts import render, get_object_or_404
 from django.views.decorators.http import require_POST
 from django.views.generic import ListView
@@ -164,9 +164,12 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            search_vector = SearchVector('title', 'body')
+            search_query = SearchQuery(query)
             results = Post.published.annotate(
-                search=SearchVector('title', 'body')
-            ).filter(search=query)
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query)
+            ).filter(search=query).order_by('-rank')
 
     return render(request,
                   'blog/post/search.html',
